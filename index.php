2c9016f409a6e056c19e5d86c60573e5510cf561
ayan-666 <67514655+ayan-666@users.noreply.github.com>
2021-06-22 22:04:14 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+			echo $_POST["name"]."<br>";
+			echo $_POST["username"]."<br>";
+			echo $_POST["password"]."<br>";
+			echo $_POST["gender"]."<br>";
+			echo $_POST["profession"]."<br>";
+			echo $_POST["bio"]."<br>";
+            $arr = $_POST["hobbies"];
+			
+			foreach($arr as $e){
+				echo "$e<br>";
+			}
+		?>
+   <html>
+	<body>
+		<form action="" method="post">
+			<table>
+				<tr>
+					<td align="right">Name</td>
+					<td>:<input name="name"  type="text"><br>
+				</tr>
+				<tr>
+					<td align="right">Username</td>
+					<td>:<input name="username" type="text" placeholder="Your name.."></td>
+				</tr>
+				<tr>
+					<td align="right">Password</td>
+					<td>:<input name="password" type="password"></td>
+				</tr>
+				<tr>
+					<td align="right">Gender</td>
+					<td>:<input type="radio" value="Male"  name="gender"> Male <input type="radio" value="Female"  name="gender"> Female <br>
+				</tr>
+				<tr>
+					<td align="right">Profession</td>
+					<td>:<select name="profession">
+							<option>Service</option>
+							<option >Business</option>
+                            <option >Teacher</option>
+					</td>
+				</tr>
+				<tr>
+					<td align="right">Hobbies</td>
+					<td>:
+						<input type="checkbox" value="Music" name="hobbies[]">Music  
+						<input type="checkbox" value="Movies" name="hobbies[]">Movies
+						<br> &nbsp;
+						<input type="checkbox" value="Sports" name="hobbies[]">Sports  
+						<input type="checkbox" value="Games" name="hobbies[]">Games
+                    </td>
+				</tr>
+				<tr>
+					<td align="right">Bio</td>
+					<td>: <textarea name="bio"></textarea>
+					</td>
+					
+				</tr>
+				<tr>
+					<td><input type="submit" value="Register"></td>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
